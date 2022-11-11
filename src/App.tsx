80c02f46216bf0016fa3348fef08923239b93e42
Peter Anyaogu <anyaogupeter601@gmail.com>
2022-11-11 18:22:36 +0100

    rainbowkit connector

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  Chain,
+  lightTheme,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
+
+const zkSync: Chain = {
+  id: 280,
+  name: "zkSync alpha testnet",
+  network: "zkSync",
+  iconUrl: "https://example.com/icon.svg",
+  iconBackground: "#fff",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "zkETH",
+  },
+  rpcUrls: {
+    default: "https://zksync2-testnet.zksync.dev",
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://explorer.zksync.io" },
+  },
+  testnet: true,
+};
+
+const { chains, provider } = configureChains(
+  [chain.goerli, zkSync],
+  [
+    jsonRpcProvider({ rpc: (chain) => ({ http: chain.rpcUrls.default }) }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Inst-Paymaster",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={{
+          lightMode: lightTheme(),
+          darkMode: darkTheme(),
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <div className="App">Inst-Paymaster</div>
+        </QueryClientProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
