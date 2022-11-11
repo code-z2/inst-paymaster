5b5bf38ab5aa3ec196eb6a906b8a2f57d7cd598d
Peter Anyaogu <anyaogupeter601@gmail.com>
2022-11-11 19:02:28 +0100

    tailwindcss and ipfs

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SyncWallet = () => {
+  return <div>SyncWallet</div>;
+};
+
+export default SyncWallet;
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    ".src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    "w-64",
+    "w-1/2",
+    "rounded-l-lg",
+    "rounded-r-lg",
+    "bg-gray-200",
+    "grid-cols-4",
+    "grid-cols-7",
+    "h-6",
+    "leading-6",
+    "h-9",
+    "leading-9",
+    "shadow-lg",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+}
