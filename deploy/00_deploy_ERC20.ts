331bcd2d5ce0e09e536535611bf8cb28082f886f
Peter Anyaogu <anyaogupeter601@gmail.com>
2022-11-12 20:14:56 +0100

    factories deployed

--- deploy/00_deploy_ERC20.ts
+++ deploy/00_deploy_ERC20.ts
@@ -1,12 +1,14 @@
 import { Wallet } from "zksync-web3";
+import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+require("dotenv").config();
 
-export default async function (hre: HardhatRuntimeEnvironment) {
+export default async function deployERC20(hre: HardhatRuntimeEnvironment) {
   const PRIVATE_KEY: any = process.env.PIVATE_KEY;
   const wallet = new Wallet(PRIVATE_KEY);
   const deployer = new Deployer(hre, wallet);
-  const ERC20Artifact = await deployer.loadArtifact("InstPayToken");
+  const ERC20Artifact = await deployer.loadArtifact("MyERC20");
 
   const factory = await deployer.deploy(ERC20Artifact, [
     "InstPayToken",
--- deploy/02_deploy_AccountFactory.ts
+++ deploy/02_deploy_AccountFactory.ts
@@ -2,8 +2,11 @@ import { utils, Wallet } from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+require("dotenv").config();
 
-export default async function (hre: HardhatRuntimeEnvironment) {
+export default async function deployAccountFactory(
+  hre: HardhatRuntimeEnvironment
+) {
   const PRIVATE_KEY: any = process.env.PIVATE_KEY;
   const wallet = new Wallet(PRIVATE_KEY);
   const deployer = new Deployer(hre, wallet);
