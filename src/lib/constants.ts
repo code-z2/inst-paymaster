f8a51228e2bc5f1207b42bb720e63832d7546974
Peter Anyaogu <anyaogupeter601@gmail.com>
2022-11-13 17:24:25 +0100

    update paymaster factory contract

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,5 +2,6 @@ export const contracts = {
   ERC20TokenAddress: "0x32A74a95429DC0e6B919B26763408b0B7788A431",
   ERC721TokenAddress: "0x5D3BF9c0B6e0eFfBc257454DAfd50Ca048bC6c76",
   SAccountFactoryAddress: "0x228a778978Fd00481B468cA7f18dbCB6A69992F8",
-  SPaymasterFactoryAddress: "0x9c19b8bdb56ca1C64ec25127161e4433eAeC4987",
+  SPaymasterFactoryAddress: "0xfA0173454eDC8d21Eb92B969C4a6Db5A3F121cDF",
+  LegacySPaymasterFactoryAddress: "0x9c19b8bdb56ca1C64ec25127161e4433eAeC4987",
 };
