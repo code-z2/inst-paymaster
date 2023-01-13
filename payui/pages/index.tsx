3ed977d3006eb0503942eadeb362666550571808
Ifeanyi Ndu <indu@fsdhgroup.com>
2023-01-13 08:31:55 +0100

    added partners

--- payui/pages/index.tsx
+++ payui/pages/index.tsx
@@ -18,7 +18,11 @@ export default function Home() {
          <meta name="og:description" content="Institutional paymaster enables users to choose any paymaster 
          they are eligible to use without handling complex interactions. " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"  />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <LandingPage />
     </>
