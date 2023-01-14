0a56048b60ef7d801d8c85101905556649994a32
Ifeanyi Ndu <indu@fsdhgroup.com>
2023-01-14 10:31:42 +0100

    added some responsiveness

--- payui/components/landingpage/landing.tsx
+++ payui/components/landingpage/landing.tsx
@@ -4,59 +4,58 @@ import { Image } from "./index";
 const LandingPage = () => {
   return (
     <div className={styles.container}>
-      <div className="h-screen p-0 m-0">
-        <nav className={styles.paynav}>
-          <a href="/" className={styles.paylogo}>
-            <Image
-              src="/assets/pm-images/logo.png"
-              alt="paymaster logo"
-              width={120}
-              height={30}
-              priority
-            />
-          </a>
+      <nav className={styles.paynav}>
+        <a href="/" className={styles.paylogo}>
+          <Image
+            src="/assets/pm-images/logo.png"
+            alt="paymaster logo"
+            width={120}
+            height={30}
+            priority
+          />
+        </a>
 
-          <ul>
-            <li>
-              <a href="#" className="mx-1 py-1">
-                Docs
-              </a>
-            </li>
-            <li>
-              <button
-                className="px-3 py-1 hover:bg-purple-600 
-            border-solid border-2 border-slate-400 rounded-md"
-              >
-                Launch App
-              </button>
-            </li>
-          </ul>
-        </nav>
-        <section className={styles.first_section}>
-          <div className={styles.heading_section}>
-            <h1>The only paymaster's access infrastructure you will need</h1>
-            <p className="text-slate-400 my-6">
-              Using paymaster to simplify zk transactions and provide
-              <br /> superior ux to dapps
-            </p>
+        <ul>
+          <li>
+            <a href="#" className="mx-1 py-1">
+              Docs
+            </a>
+          </li>
+          <li>
             <button
-              className="px-3 py-1 w-40 hover:bg-purple-900 bg-purple-600 
+              className="px-3 py-1 w-30 hover:bg-purple-600 
             border-solid border-2 border-slate-400 rounded-md"
             >
               Launch App
             </button>
-          </div>
-          <div className={styles.heading_image}>
-            <Image
-              src="/assets/pm-images/header_image.png"
-              alt="paymaster-planet-logo"
-              width={570}
-              height={600}
-              priority
-            />
-          </div>
-        </section>
-      </div>
+          </li>
+        </ul>
+      </nav>
+      <section className={styles.first_section}>
+        <div className={styles.heading_section}>
+          <h1>The only paymaster's access infrastructure you will need</h1>
+          <p className="text-slate-400 my-6">
+            Using paymaster to simplify zk transactions and provide
+            <br /> superior ux to dapps
+          </p>
+          <button
+            className="px-3 py-1 w-40 hover:bg-purple-900 bg-purple-600 
+            border-solid border-2 border-slate-400 rounded-md"
+          >
+            Launch App
+          </button>
+        </div>
+        <div className={styles.heading_image}>
+          <Image
+            src="/assets/pm-images/header_image.png"
+            alt="paymaster-planet-logo"
+            width={570}
+            height={600}
+            priority
+          />
+        </div>
+      </section>
+
       <section className={styles.second_section}>
         <div className={styles.chain_section}>
           <h2>On-Chain Rewards Middleware</h2>
