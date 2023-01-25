6f0753c531d44bccf6ed1b2109c7ab1d4a4d4bbe
Ifeanyi Ndu <indu@fsdhgroup.com>
2023-01-25 14:31:57 +0100

    added scaling 100 - 125

--- packages/client/components/index.tsx
+++ packages/client/components/index.tsx
@@ -4,7 +4,7 @@ export { default as Auth } from "./auth/private";
 export { default as Footer } from "./landingpage/footer";
 export { default as Image } from "next/image";
 
-export { default as ActivePaymaster } from "./activepaymaster/activePaymasterPage";
+export { default as PaymastersList } from "./activepaymaster/paymastersListPage";
 export { default as ActivePaymasterFirstSection } from "./activepaymaster/firstSection";
 export { default as AllPaymasters } from "./activepaymaster/allpaymasters";
 export { default as Loadmore } from "./activepaymaster/loadmore";
